4237e93d28d75ec0ab80ad4e74c80fce1f5ff1a6
Stas <smaster950@gmail.com>
2023-01-02 13:53:58 +0300

    Complete Exercise_4 and add Exercise_4 text file

---
+++ Exercise_4.py
@@ -0,0 +1,25 @@
+# Задана натуральная степень k.
+# Сформировать случайным образом список коэффициентов (значения от 0 до 100) многочлена и записать в файл многочлен степени k.
+# k=2 => 2*x² + 4*x + 5 = 0 или x² + 5 = 0 или 10*x² = 0
+
+from random import randint
+
+k = int(input('Input your number: '))
+
+c = []
+
+for i in range(k+1):
+    c.append(randint(0, 100))
+
+p = str(c[0])
+
+for j in range(1, k+1):
+    p = str(c[j]) + '*x^' + str(j) + ' ' + '+' + ' ' + p
+
+p1 = p + ' = 0'
+
+print(p1)
+
+data = open('Exercise_4.txt', 'w')
+data.writelines(p1)
+data.close()
